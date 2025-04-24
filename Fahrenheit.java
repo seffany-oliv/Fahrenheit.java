a15e6bf220f0f4d3d15576e7a1c7032834a31573
seffany-oliv <steffany.oliv1@gmail.com>
2025-04-24 14:27:50 -0300

    commit inicial da steffany

--- Fahrenheit.java
+++ Fahrenheit.java
@@ -4,21 +4,43 @@ public class Fahrenheit {
     public static void main(String[] args) {
         //Declaração de variávais
         double cel, fah; //cel - temperatura em graus celsius; fah - temperatura em graus fahrenheit
+        int op; //Opição do usuário
 
-        //Criação e instâcia de objeto de entrada
+        //Criação e instâcia do objeto de entrada
         Scanner entrada = new Scanner(System.in);
 
         //Apresentação do programa
         System.out.println("\n\t\t\t -- Conversor -- \n");
+       
+        //Menu
+        System.out.println("1. ºC para ºF");
+        System.out.println("2. ºF para ºC");
+        System.out.println("Sair");
+
+        //Opção do usuário
+        System.out.println("\nOpção");
+        op = entrada.nextInt();
+
+        if(op == 1){
+            System.out.println("Conversão para fahrenheit");
+
+            //Entradas
+            System.out.println("Informe a temperatura em ºC: ");
+            cel = entrada.nextInt();
+
+            //Processamento
+            fah = cel * 9 / 5 + 32;
+
+            //Saída
+            System.out.println("\n" + cel + "ºC =" + fah + "ºF\n");
+        }else if(op == 2){
+            //Converter Fahrenheit para Celsius
+        }else if(op == 3) {
+            System.out.println("Forte abraço!!!");
+            
+        }else {
+            System.out.println("Opção" + op + "incorreta!");
+        }
         
-        //Entradas
-        System.out.println("Informe a temperatura em ºC: ");
-        cel = entrada.nextDouble();
-
-        //Processamento
-        fah = cel * 9 / 5 + 32;
-
-        //Saída
-        System.out.println("\n" + cel + "ºC =" + fah + "ºF\n");
     }
 }
